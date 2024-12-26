584c70bc72f5c139ea223c444c07a9ba0cb35610
Chin-Su <viet129@hotmail.com>
2024-12-26 13:51:02 +0700

    Update Link Locket

--- remove_header.js
+++ remove_header.js
@@ -1,4 +1,5 @@
-// Remove unnecessary headers
-delete $request.headers["Authorization"];
-delete $request.headers["X-RevenueCat-Token"];
-$done({ headers: $request.headers });
+
+const version = 'V1.0.2';
+
+
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
